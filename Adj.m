4f0b80d96eb830d400bdad578e12ff6d7c033186
Tianchen Song <sf995511sf@qq.com>
2023-04-18 22:37:47 -0400

    Update ur5Jb

--- Adj.m
+++ Adj.m
@@ -2,5 +2,5 @@ function Ad = Adj(g)
     R = g(1:3,1:3);
     p = g(1:3,4);
     Ad = [R SKEW3(p)*R;
-        eye(3) R];
+        zeros(3,3) R];
 end
--- ur5BodyJacobian.m
+++ ur5BodyJacobian.m
@@ -22,21 +22,21 @@ q6 = [0.425+0.392;0.1093;0.0892-0.09475];
 
 g0 = [-1,0,0,0.425+0.392;0,0,1,0.0825+0.1093;0,1,0,0.0892-0.09475;0,0,0,1];
 
-rot1 = [SKEW3(w1) -cross(w1,q1) ;0 0 0 1];
-rot2 = [SKEW3(w2) -cross(w2,q2) ;0 0 0 1];
-rot3 = [SKEW3(w3) -cross(w3,q3) ;0 0 0 1];
-rot4 = [SKEW3(w4) -cross(w4,q4) ;0 0 0 1];
-rot5 = [SKEW3(w5) -cross(w5,q5) ;0 0 0 1];
-rot6 = [SKEW3(w6) -cross(w6,q6) ;0 0 0 1];
+rot1 = expm([SKEW3(w1) -cross(w1,q1) ;0 0 0 0]*theta(1,1));
+rot2 = expm([SKEW3(w2) -cross(w2,q2) ;0 0 0 0]*theta(2,1));
+rot3 = expm([SKEW3(w3) -cross(w3,q3) ;0 0 0 0]*theta(3,1));
+rot4 = expm([SKEW3(w4) -cross(w4,q4) ;0 0 0 0]*theta(4,1));
+rot5 = expm([SKEW3(w5) -cross(w5,q5) ;0 0 0 0]*theta(5,1));
+rot6 = expm([SKEW3(w6) -cross(w6,q6) ;0 0 0 0]*theta(6,1));
 
 %% calculate body jacobian
 fk = rot1*rot2*rot3*rot4*rot5*rot6*g0;
-Xi1 = [q1;w1];
-Xi2 = [q2;w2];
-Xi3 = [q3;w3];
-Xi4 = [q4;w4];
-Xi5 = [q5;w5];
-Xi6 = [q6;w6];
+Xi1 = [-cross(w1,q1);w1];
+Xi2 = [-cross(w2,q2);w2];
+Xi3 = [-cross(w3,q3);w3];
+Xi4 = [-cross(w4,q4);w4];
+Xi5 = [-cross(w5,q5);w5];
+Xi6 = [-cross(w6,q6);w6];
 Js = [Xi1 Adj(rot1)*Xi2 Adj(rot1*rot2)*Xi3 Adj(rot1*rot2*rot3)*Xi4 Adj(rot1*rot2*rot3*rot4)*Xi5 Adj(rot1*rot2*rot3*rot4*rot5)*Xi6];
 Jb = Adj(fk)^(-1)*Js;
 end
